5f4042e1220584be473472eb8ce810c8a976a8c2
Dirk Fortmeier <mail@dfortmeier.de>
2025-01-13 20:23:02 +0100

    feat: add glfw

---
+++ src/backend_vulkan.cpp
@@ -0,0 +1,15 @@
+#include <GLFW/glfw3.h>
+
+#include "klartraum/backend_vulkan.hpp"
+
+namespace klartraum {
+
+BackendVulkan::BackendVulkan() {
+    // Constructor
+}
+
+BackendVulkan::~BackendVulkan()
+{
+}
+
+} // namespace klartraum
