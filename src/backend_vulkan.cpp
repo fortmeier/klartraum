5afbabdfb6d44311c0b803e6b4406f47baa20fa2
Dirk Fortmeier <mail@dfortmeier.de>
2025-01-14 21:32:14 +0100

    feat: add glfw window creation and main loop

--- src/backend_vulkan.cpp
+++ src/backend_vulkan.cpp
@@ -1,3 +1,5 @@
+#include <stdexcept>
+
 #include <GLFW/glfw3.h>
 
 #include "klartraum/backend_vulkan.hpp"
@@ -10,6 +12,30 @@ BackendVulkan::BackendVulkan() {
 
 BackendVulkan::~BackendVulkan()
 {
+}
+
+void BackendVulkan::initialize() {
+    // Initialize Vulkan
+    if (!glfwInit()) {
+        throw std::runtime_error("Failed to initialize GLFW");
+    }
+}
+
+void BackendVulkan::loop() {
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+}
+
+void BackendVulkan::shutdown() {
+    // Shutdown Vulkan
+    glfwTerminate();
 }
 
 } // namespace klartraum
