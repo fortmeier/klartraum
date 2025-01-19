647eeed4d65ec44acc36ad0375ad4f327d6d948d
Dirk Fortmeier <mail@dfortmeier.de>
2025-01-19 14:31:51 +0100

    feat: towards integration and separation of basic vulkan tutorial code

--- src/vulkan_gaussian_splatting.cpp
+++ src/vulkan_gaussian_splatting.cpp
@@ -3,7 +3,7 @@
 
 namespace klartraum {
 
-VulkanGaussianSplatting::VulkanGaussianSplatting(BackendVulkan &backendVulkan) : device(backendVulkan.getDevice()) {
+VulkanGaussianSplatting::VulkanGaussianSplatting(BackendVulkan &backendVulkan) : backendVulkan(backendVulkan) {
     createCommandPool();
     createCommandBuffers();
 }
@@ -12,10 +12,27 @@ VulkanGaussianSplatting::~VulkanGaussianSplatting() {
 
 }
 
+void VulkanGaussianSplatting::drawFrame() {
+    // TODO: continue copying from tutorial
+    
+}
+
 void VulkanGaussianSplatting::createCommandPool() {
+    auto device = backendVulkan.getDevice();
+
+    VkCommandPoolCreateInfo poolInfo{};
+    poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    poolInfo.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+    poolInfo.queueFamilyIndex = backendVulkan.getQueueFamilyIndices().graphicsFamily.value();
+
+    if (vkCreateCommandPool(device, &poolInfo, nullptr, &commandPool) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create command pool!");
+    }    
 }
 
 void VulkanGaussianSplatting::createCommandBuffers() {
+    auto device = backendVulkan.getDevice();
+
     commandBuffers.resize(BackendVulkan::MAX_FRAMES_IN_FLIGHT);
     VkCommandBufferAllocateInfo allocInfo{};
     allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
