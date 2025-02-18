82a9807ff7fb2e576b4f234b8d3549230467e863
Dirk Fortmeier <mail@dfortmeier.de>
2025-02-18 21:03:05 +0100

    fix issue with the camera UBO usable in compute shader

--- src/camera.cpp
+++ src/camera.cpp
@@ -56,7 +56,7 @@ void Camera::createDescriptorSetLayout() {
     uboLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
     uboLayoutBinding.descriptorCount = 1;
 
-    uboLayoutBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+    uboLayoutBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_COMPUTE_BIT;
 
     VkDescriptorSetLayoutCreateInfo layoutInfo{};
     layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
