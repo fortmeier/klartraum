8f587855c2780fc3caf101bb9a110f5649d6d118
Dirk Fortmeier <mail@dfortmeier.de>
2025-03-06 20:13:20 +0100

    added class for buffer and tensor

---
+++ include/klartraum/vulkan_buffer.hpp
@@ -0,0 +1,57 @@
+#ifndef KLARTRAUM_VULKAN_BUFFER_HPP
+#define KLARTRAUM_VULKAN_BUFFER_HPP
+
+#include <vulkan/vulkan.h>
+
+#include <klartraum/vulkan_kernel.hpp>
+
+namespace klartraum {
+
+template <typename T>
+class VulkanBuffer {
+public:
+    VulkanBuffer(VulkanKernel& kernel, uint32_t size) : vulkanKernel(kernel), size(size) { //, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties);
+        auto& device = kernel.getDevice();
+
+        VkBufferCreateInfo bufferInfo{};
+        bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+        bufferInfo.size = sizeof(T) * size;
+        bufferInfo.usage = VK_BUFFER_USAGE_STORAGE_BUFFER_BIT ;
+        bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    
+        if (vkCreateBuffer(device, &bufferInfo, nullptr, &vertexBuffer) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create compute buffer!");
+        }
+
+        VkMemoryRequirements memRequirements;
+        vkGetBufferMemoryRequirements(device, vertexBuffer, &memRequirements);
+    
+        VkMemoryAllocateInfo allocInfo{};
+        allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+        allocInfo.allocationSize = memRequirements.size;
+        allocInfo.memoryTypeIndex = vulkanKernel.findMemoryType(memRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+    
+        if (vkAllocateMemory(device, &allocInfo, nullptr, &vertexBufferMemory) != VK_SUCCESS) {
+            throw std::runtime_error("failed to allocate vertex buffer memory!");
+        }        
+    }
+
+    ~VulkanBuffer() {
+        auto& device = vulkanKernel.getDevice();
+        vkDestroyBuffer(device, vertexBuffer, nullptr);
+        vkFreeMemory(device, vertexBufferMemory, nullptr);
+    }
+
+private:
+    const uint32_t size;
+
+    VkBuffer vertexBuffer;
+    VkDeviceMemory vertexBufferMemory;
+    VulkanKernel& vulkanKernel;
+
+
+};
+
+} // namespace klartraum
+
+#endif // KLARTRAUM_VULKAN_BUFFER_HPP
---
+++ include/klartraum/vulkan_tensor.hpp
@@ -0,0 +1,32 @@
+#ifndef KLARTRAUM_VULKAN_TENSOR_HPP
+#define KLARTRAUM_VULKAN_TENSOR_HPP
+
+#include "klartraum/vulkan_buffer.hpp"
+
+
+namespace klartraum {
+
+template <typename T>
+class VulkanTensor {
+public:
+    VulkanTensor(VulkanKernel& kernel, uint32_t batch, uint32_t depth=1, uint32_t height=1, uint32_t width=1) :
+        buffer(kernel, batch * depth * height * width),
+        batch(batch),
+        depth(depth),
+        height(height),
+        width(width)
+    {
+
+    }
+
+private:
+    VulkanBuffer<typename T> buffer;
+    uint32_t batch;
+    uint32_t depth;
+    uint32_t height;
+    uint32_t width;
+};
+
+} // namespace klartraum
+
+#endif // KLARTRAUM_VULKAN_TENSOR_HPP
