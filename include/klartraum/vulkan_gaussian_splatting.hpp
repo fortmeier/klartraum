219f354e70b058bdd515bc40f16d09d88a700035
Dirk Fortmeier <mail@dfortmeier.de>
2025-06-20 15:32:03 +0200

    further cleanup of vulkan gaussian splatting (header)

--- include/klartraum/vulkan_gaussian_splatting.hpp
+++ include/klartraum/vulkan_gaussian_splatting.hpp
@@ -1,41 +1,45 @@
 #ifndef VULKAN_GAUSSIAN_SPLATTING_HPP
 #define VULKAN_GAUSSIAN_SPLATTING_HPP
 
-#include <vector>
 #include <string>
+#include <vector>
+
 #include <glm/gtc/matrix_transform.hpp>
 
+#include "klartraum/drawgraph/buffertransformation.hpp"
+#include "klartraum/drawgraph/drawgraphgroup.hpp"
+#include "klartraum/drawgraph/imageviewsrc.hpp"
 #include "klartraum/drawgraph/rendergraphelement.hpp"
-
 #include "klartraum/vulkan_buffer.hpp"
 #include "klartraum/vulkan_gaussian_splatting_types.hpp"
 
-#include "klartraum/drawgraph/drawgraphgroup.hpp"
-#include "klartraum/drawgraph/buffertransformation.hpp"
-#include "klartraum/drawgraph/imageviewsrc.hpp"
-
 namespace klartraum {
-
 
 enum class GaussianSplattingRenderingType {
     PointCloud,
     // GaussianSplatting, not implemented yet
-};    
+};
 
 class VulkanGaussianSplatting : virtual public RenderGraphElement, virtual public DrawGraphGroup {
-/**
- * @brief 
- * 
- * Gaussian Splatting consists of these steps:
- * 1. project the 3D Gaussian to 2D
- * 2. distribute the 2D Gaussians to 4x4 subtiles
- * 3. sort the 2D Gaussians by depth and tile using radix sort
- * 4. splat the 2D Gaussians to each subtile of the image
- * 
- * Here, this is solved by a drawgraph consisting of
- * 1. shader drawgraphelent (projection shader, 3D gaussians in, 2D gaussians out)
- * 2. sorting shader (2D gaussians in, sorted 2D gaussians out)
- */
+    /**
+     * @brief
+     *
+     * Gaussian Splatting consists of these steps:
+     * 1. project the 3D Gaussian to 2D
+     * 2. distribute/bin the 2D Gaussians to 4x4 subtiles
+     * 3. sort the 2D Gaussians by depth and tile using radix sort
+     * 4. splat the 2D Gaussians to each subtile of the image
+     *
+     * The current implementation is probably not optimal:
+     * - projection and binning could be combined into single step
+     * - binning of the 2D gaussians creates a new number of gaussians,
+     *   which is limited to 2 * number of initinal 3D gaussians.
+     *   (2 is a magic number currently)
+     * - all further steps thus start enough workgroups to potentially
+     *   process all newly created gaussians and discarding the ones
+     *   that are not needed. a dynamic workgroup count
+     *   would be more efficient, but it is unclear how to implement this
+     */
 public:
     VulkanGaussianSplatting(
         VulkanKernel& vulkanKernel,
@@ -44,20 +48,17 @@ public:
         std::string path);
     ~VulkanGaussianSplatting();
 
-    virtual void checkInput(DrawGraphElementPtr input, int index = 0);
+    virtual void checkInput(DrawGraphElementPtr input, int index = 0) override;
 
-    virtual void _setup(VulkanKernel& vulkanKernel, uint32_t numberPaths);
+    virtual void _setup(VulkanKernel& vulkanKernel, uint32_t numberPaths) override;
 
-    virtual void _record(VkCommandBuffer commandBuffer, uint32_t pathId);
+    virtual void _record(VkCommandBuffer commandBuffer, uint32_t pathId) override;
 
-    virtual const char* getType() const
-    {
+    virtual const char* getType() const override {
         return "GaussianSplatting";
     }
-    
 
 private:
-
     void loadSPZModel(std::string path);
 
     VulkanKernel* vulkanKernel = nullptr;
@@ -70,15 +71,13 @@ private:
 
     uint32_t numberOfPaths = 0;
 
-
     std::shared_ptr<BufferElementSinglePath<Gaussian3DBuffer>> gaussians3D;
-    
+
     std::shared_ptr<GaussianProjection> project3Dto2D;
     std::shared_ptr<GaussianSort> sort2DGaussians;
     std::shared_ptr<GaussianBinning> bin;
     std::shared_ptr<GaussianComputeBounds> computeBounds;
     std::shared_ptr<GaussianSplatting> splat;
-   
 };
 
 } // namespace klartraum
