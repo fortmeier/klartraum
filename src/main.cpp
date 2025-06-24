9289d4f15c72187542f611756d0f55559e785585
Dirk Fortmeier <mail@dfortmeier.de>
2025-06-24 21:01:35 +0200

    small rename in main (core->engine)

--- src/main.cpp
+++ src/main.cpp
@@ -11,26 +11,26 @@ int main() {
 
     klartraum::GlfwFrontend frontend;
 
-    auto& core = frontend.getKlartraumEngine();
+    auto& engine = frontend.getKlartraumEngine();
 
-    klartraum::RenderPassPtr renderpass = core.createRenderPass();
+    klartraum::RenderPassPtr renderpass = engine.createRenderPass();
 
     std::shared_ptr<klartraum::DrawBasics> axes = std::make_shared<klartraum::DrawBasics>(klartraum::DrawBasicsType::Axes);
     renderpass->addDrawComponent(axes);
 
-    auto& vulkanContext = core.getVulkanContext();
+    auto& vulkanContext = engine.getVulkanContext();
     
     auto& cameraUBO = renderpass->getCameraUBO();
     std::string spzFile = "./3rdparty/spz/samples/hornedlizard.spz";
     std::shared_ptr<klartraum::VulkanGaussianSplatting> splatting = vulkanContext.create<klartraum::VulkanGaussianSplatting>(renderpass, cameraUBO, spzFile);
     
-    core.add(splatting);
+    engine.add(splatting);
 
     std::shared_ptr<klartraum::InterfaceCameraOrbit> cameraOrbit = std::make_shared<klartraum::InterfaceCameraOrbit>(klartraum::InterfaceCameraOrbit::UpDirection::Y);
     cameraOrbit->setAzimuth(-31.0);
     cameraOrbit->setElevation(-0.5);
-    core.setInterfaceCamera(cameraOrbit);
-    core.setCameraUBO(cameraUBO);
+    engine.setInterfaceCamera(cameraOrbit);
+    engine.setCameraUBO(cameraUBO);
 
     frontend.loop();
 
