d68c2952d392d1143d38a84698edcb7ec86f03a4
Dirk Fortmeier <mail@dfortmeier.de>
2025-07-22 20:30:58 +0200

    add comments and template method for compute graph element

--- include/klartraum/computegraph/computegraphelement.hpp
+++ include/klartraum/computegraph/computegraphelement.hpp
@@ -6,6 +6,22 @@
 
 #include "klartraum/vulkan_context.hpp"
 
+/**
+ * @brief Abstract base class representing an element in a compute graph.
+ * 
+ * ComputeGraphElement serves as the foundation for building computational graphs
+ * in the Klartraum framework. Each element can have multiple inputs and outputs,
+ * forming a directed acyclic graph structure for computation pipelines.
+ * 
+ * 
+ * @note This class is designed to work with shared_ptr (ComputeGraphElementPtr)
+ *       for automatic memory management and safe reference handling.
+ *       Generally, you should not create instances of this class directly,
+ *       but rather derive from it to implement specific compute graph elements and
+ *       use the VulkanContext::create to instantiate them.
+ * 
+ * @see ComputeGraph which is used to compile a compute graph from these elements.
+ */
 namespace klartraum {
 
 class ComputeGraphElement;
@@ -14,23 +30,14 @@ typedef std::shared_ptr<ComputeGraphElement> ComputeGraphElementPtr;
 
 class ComputeGraph;
 
-/**
- * @brief Sets the input element for this ComputeGraphElement at the specified index.
- * 
- * If slot is used, this indicates that the input is not given by input directly,
- * but rather by the slot element of the given element.
- * 
- * If slot is specified, retrieves the input element from the provided input's slot and checks its validity.
- * Updates the internal input and source output slot mappings.
- * 
- * @param input The ComputeGraphElementPtr to set as input.
- * @param index The index at which to set the input (default is 0).
- * @param slot The slot of the input element to use (default is -1, meaning no slot).
- */
+
 class ComputeGraphElement {
 public:
     friend class ComputeGraph;
 
+    // maybe order of arguments should be changed to index, input, slot
+    // or index should be a template parameter
+    [[deprecated("Consider using template version setInput<index>(input, slot) for better type safety and clarity")]]
     void setInput(ComputeGraphElementPtr input, int index = 0, int slot = -1) {
         if(slot == -1) {
             checkInput(input, index);
@@ -39,6 +46,24 @@ public:
         }
         inputs[index] = input;
         srcOutputSlots[index] = slot;
+    }
+
+    /**
+     * @brief Sets the input element for this ComputeGraphElement at the specified index.
+     * 
+     * If slot is used, this indicates that the input is not given by input directly,
+     * but rather by the slot element of the given element.
+     * 
+     * If slot is specified, retrieves the input element from the provided input's slot and checks its validity.
+     * Updates the internal input and source output slot mappings.
+     * 
+     * @tparam index The index at which to set the input (default is 0).
+     * @param input The ComputeGraphElementPtr to set as input.
+     * @param slot The slot of the input element to use (default is -1, meaning no slot). See explanation of slots above.
+     */    
+    template<int index = 0>
+    void setInput(ComputeGraphElementPtr input, int slot = -1) {
+        setInput(input, index, slot);
     }
 
     virtual void checkInput(ComputeGraphElementPtr input, int index = 0) {
