6d274f912baf3e072bd405f7942c418df699cb01
Dirk Fortmeier <mail@dfortmeier.de>
2025-09-18 20:25:25 +0200

    use unique_ptr in vector of compute graphs

--- src/klartraum_engine.cpp
+++ src/klartraum_engine.cpp
@@ -40,8 +40,8 @@ void KlartraumEngine::step() {
     auto& graphicsQueue = vulkanContext.getGraphicsQueue();
 
     VkSemaphore renderFinishedSemaphore;
-    for(auto &computeGraph : computeGraphs) {
-        renderFinishedSemaphore = computeGraph.submitTo(graphicsQueue, imageIndex);
+    for (auto& computeGraph : computeGraphs) {
+        renderFinishedSemaphore = computeGraph->submitTo(graphicsQueue, imageIndex);
     }
 
     // finish frame rendering
@@ -68,9 +68,9 @@ VulkanContext& KlartraumEngine::getVulkanContext()
 
 void KlartraumEngine::add(ComputeGraphElementPtr element)
 {
-    computeGraphs.emplace_back(vulkanContext, 3);
+    computeGraphs.emplace_back(std::make_unique<ComputeGraph>(vulkanContext, 3));
     auto& computeGraph = computeGraphs.back();
-    computeGraph.compileFrom(element);
+    computeGraph->compileFrom(element);
 }
 
 RenderPassPtr KlartraumEngine::createRenderPass()
